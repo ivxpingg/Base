d16f7f2036effdf53d48b63b28072d8cd4ffa0db
sgj <764341699@qq.com>
2016-04-12 09:15:14 +0800

    大盒子首页

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,60 +3,45 @@ module.exports = function(grunt){
     'use strict';
 	
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		banner: '',
+		//pkg: grunt.file.readJSON('package.json'),
+		//banner: '',
         
         watch: {
 	      // If any .less file changes in directory "build/less/" run the "less"-task.
 	      // files: ["build/less/*.less", "build/less/skins/*.less", "dist/js/app.js"],
-	      files: ["build/scss/*.scss", "build/scss/skins/*.scss", "dist/js/app.js"],
-	      tasks: ["sass", "uglify"]
+	      files: ["build/scss/pages/*.scss"],
+	      tasks: ["sass:development","autoprefixer"]
 	    },
 
         // SASS compiler
 	    sass: {
+	      // bootstrap: {
+	      // 	options: {
+	      //     style: 'expanded' //'compressed'
+	      //   },
+	      //   files: {
+	      //     'bootstrap/css/bootstrap.css': 'bootstrap/scss/bootstrap.scss'
+	      //   }
+	      // },
 	      development: {
 	        options: {
-	          style: 'expanded'
+	          style: 'expanded' //'compressed'
 	        },
-	        files: {
-	          'bootstrap/css/bootstrap.css': 'bootstrap/scss/bootstrap.scss'
-	        }
+	        files: {'dist/css/home.css': 'build/scss/pages/home.scss'}
 	      },
 	      production: {
 	        options: {
 	          style: 'compressed'
 	        },
-	        files: {
-	          'bootstrap/css/bootstrap.min.css': 'bootstrap/scss/bootstrap.scss'
-	        }
+	        files: {'dist/css/home.css': 'build/scss/pages/home.scss'}
 	      }
 	    },
 
-	    //校验js
-	    jshint: {
-	    	options: {
-	    		jshintrc: '.jshintrc'
-	    	},
-
-	    	files: {
-	    		src: ''
-	    	}
-	    },
-
-
-	    //合并文件
-	    concat: {},
-
-	    //压缩文件
-	    uglify: {
-	    	options: {}
-	    },
-
+	    //
 	    autoprefixer: {
 	    	dist: {
 	    		files: {
-	    			'test/css/auto.css': 'test/css/autoprefixer.css'
+	    			'dist/css/home.css': 'dist/css/home.css'
 	    		}
 	    	}
 	    }
@@ -65,12 +50,12 @@ module.exports = function(grunt){
 
     grunt.loadNpmTasks('grunt-contrib-watch');    //监测
     grunt.loadNpmTasks('grunt-contrib-sass');     //css编译
-    grunt.loadNpmTasks('grunt-contrib-uglify');   //压缩
-    grunt.loadNpmTasks('grunt-contrib-clean');    //清除
-    grunt.loadNpmTasks('grunt-contrib-concat');   //合并
-    grunt.loadNpmTasks('grunt-contrib-jshint');   //检验js
+    // grunt.loadNpmTasks('grunt-contrib-uglify');   //压缩
+    // grunt.loadNpmTasks('grunt-contrib-clean');    //清除
+    // grunt.loadNpmTasks('grunt-contrib-concat');   //合并
+    // grunt.loadNpmTasks('grunt-contrib-jshint');   //检验js
     grunt.loadNpmTasks('grunt-autoprefixer');     //css自动添加浏览器前缀(后处理程序)
 
     //
-    grunt.registerTask('default',['autoprefixer']);    
+    grunt.registerTask('default',['watch']);    
 };
